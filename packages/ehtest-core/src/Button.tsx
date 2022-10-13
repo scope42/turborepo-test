90811dd3051fa857b0578fedf8e9ed06be9dd2f9
Erik Hofer <erik-hofer@mail.de>
2022-10-13 19:38:59 +0200

    feat(core): make buttons cooler

--- packages/ehtest-core/src/Button.tsx
+++ packages/ehtest-core/src/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <button>😎 {props.children}</button>;
 }
 
 Button.displayName = "Button";
